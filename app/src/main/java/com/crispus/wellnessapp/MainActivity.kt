bf01c7267e63bee3ddf6da02e911b32acd6f021b
Crispus-byt <crispusbusiness@gmail.com>
2026-04-02 13:45:56 +0300

    first commit

---
+++ app/src/main/java/com/crispus/wellnessapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.crispus.wellnessapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        healthy recipe intent
+//        finding the views from the layout using their id's
+
+
+//        variable to store butttons
+
+        val recipe=findViewById<Button>(R.id.recipes)
+
+//        set onclick listener
+
+        recipe.setOnClickListener {
+//            writing intents
+            val recipeIntent= Intent(applicationContext, HealthyRecipes::class.java)
+
+            startActivity(recipeIntent)
+        }
+//
+//        meditation intent
+
+        val meditation=findViewById<Button>(R.id.meditation)
+
+        meditation.setOnClickListener {
+
+            val meditationIntent= Intent(applicationContext,Meditation::class.java)
+
+            startActivity(meditationIntent)
+        }
+
+//        nutrition intent
+
+        val nutrition=findViewById<Button>(R.id.nutrition)
+
+
+        nutrition.setOnClickListener {
+
+            val nutritionIntent=Intent(applicationContext, NutritionAdvice::class.java)
+
+            startActivity(nutritionIntent)
+        }
+
+
+
+    }
+}
