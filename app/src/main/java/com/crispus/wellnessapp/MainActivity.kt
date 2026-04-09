b53cf7a0888d7a1605db632d3ba096fa74d648b1
Crispus-byt <crispusbusiness@gmail.com>
2026-04-09 11:26:56 +0300

    android pts updates

--- app/src/main/java/com/crispus/wellnessapp/MainActivity.kt
+++ app/src/main/java/com/crispus/wellnessapp/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.crispus.wellnessapp
 
 import android.content.Intent
+//import android.media.tv.AdRequest
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
 
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 
 class MainActivity : AppCompatActivity() {
+
+//    variable to store ad once it loads
+    private  var mInterstitialAd : InterstitialAd?=null
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this)
+        val adView=findViewById<AdView>(R.id.adView)
+        val adRequest= AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+
+//        loading interstitial ad
+                loadInterstitialAd()
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -35,6 +60,9 @@ class MainActivity : AppCompatActivity() {
             val recipeIntent = Intent(applicationContext, HealthyRecipes::class.java)
 
             startActivity(recipeIntent)
+
+//            interstitial ad
+            showInterstitialAd()
         }
 //
 //        meditation intent
@@ -46,6 +74,8 @@ class MainActivity : AppCompatActivity() {
             val meditationIntent = Intent(applicationContext, Meditation::class.java)
 
             startActivity(meditationIntent)
+
+            showInterstitialAd()
         }
 
 //        nutrition intent
@@ -110,4 +140,32 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    fun loadInterstitialAd() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712", // Test ID
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    mInterstitialAd = ad
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    mInterstitialAd = null
+                }
+            }
+        )
+    }
+    //Show Interstitial ad
+    fun showInterstitialAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        }
+
+    }
+
 }
